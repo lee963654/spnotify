6923e5e08db63d557cb3f3f54aa509a4a2b63d71
James Lee <lee963654@gmail.com>
2023-06-13 17:53:58 -0400

    starting to add user seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,6 +6,12 @@ from sqlalchemy.sql import text
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
+    demo_two = User(
+        username="Demo_Two", email="demo_two@aa.io", password="password"
+    )
+    john = User(
+        username="Johnny", email="john@aa.io", password="password"
+    )
     marnie = User(
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
@@ -14,6 +20,8 @@ def seed_users():
     db.session.add(demo)
     db.session.add(marnie)
     db.session.add(bobbie)
+    db.session.add(demo_two)
+    db.session.add(john)
     db.session.commit()
 
 
@@ -28,5 +36,5 @@ def undo_users():
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
