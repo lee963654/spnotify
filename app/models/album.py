fcf824dd13c33acc041712f9a963eb7315be1256
James Lee <lee963654@gmail.com>
2023-06-14 12:17:01 -0400

    fixed seed data

--- app/models/album.py
+++ app/models/album.py
@@ -7,7 +7,7 @@ class Album(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(255), nullable=False, unique=True)
     artist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("artists.id")), nullable=False)
     album_picture = db.Column(db.String(255))
 
