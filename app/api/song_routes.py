bfa666563c2adba7d19873cf0376dcad0f1248f1
James Lee <lee963654@gmail.com>
2023-06-19 20:27:26 -0400

    working on audioplayer store

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,10 +1,85 @@
 from flask import Blueprint, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.api.auth_routes import validation_errors_to_error_messages
-from app.models import Album, AlbumReview, db, Song
+from app.models import Album, AlbumReview, db, Song, Artist, Playlist
 
 
 song_routes = Blueprint("songs", __name__)
+
+
+@song_routes.route("/playlist/<int:playlist_id>")
+@login_required
+def play_playlist_songs(playlist_id):
+    """
+    Play all songs in a playlist
+    """
+    playlist = Playlist.query.filter(Playlist.id == playlist_id)
+    playlist_dict = playlist.to_dict()
+    playlist_dict_res = [song.to_dict() for song in playlist_dict["songs_in_playlist"].values()]
+    for song in playlist_dict_res:
+        artist = Artist.query.filter(Artist.id == song.artist_id)
+        artist_dict = artist.to_dict()
+        album = Album.query.filter(Album.id == song.album_id)
+        album_dict = album.to_dict()
+        song["album"] = album_dict
+        song["artist"] = artist_dict
+    return playlist_dict_res
+
+
+@song_routes.route("/artist/<int:artist_id>")
+@login_required
+def play_artist_songs(artist_id):
+    """
+    Play all songs by artist
+    """
+
+    songs = Song.query.filter(Song.artist_id == artist_id)
+    artist = Artist.query.filter(Artist.id == artist_id)
+    artist_dict = artist.to_dict()
+    songs_dict = [song.to_dict() for song in songs]
+    for song in songs_dict:
+        album = Album.query.filter(Album.id == song.album_id)
+        album_dict = album.to_dict()
+        song["album"] = album_dict
+        song["artist"] = artist_dict
+    return songs_dict
+
+
+@song_routes.route("/album/<int:album_id>")
+@login_required
+def play_album_songs(album_id):
+    """
+    Play all songs on an album
+    """
+    songs = Song.query.filter(Song.album_id == album_id)
+    album = Album.query.filter(Album.id == album_id)
+    artist = Artist.query.filter(Artist.id == album.artist_id)
+    album_dict = album.to_dict()
+    artist_dict = artist.to_dict()
+    songs_dict = [song.to_dict() for song in songs]
+    for song in songs_dict:
+        song["album"] = album_dict
+        song["artist"] = artist_dict
+    return songs_dict
+
+
+@song_routes.route("/<int:song_id>")
+@login_required
+def play_song(song_id):
+    """
+    Play one song
+    """
+
+    song = Song.query.get(song_id)
+    album = Album.query.filter(Album.id == song.album_id)
+    artist = Artist.query.filter(Artist.id == song.artist_id)
+    artist_dict = artist.to_dict()
+    album_dict = album.to_dict()
+    song_dict = song.to_dict()
+    song_dict["album"] = album_dict
+    song_dict["artist"] = artist_dict
+
+    return song_dict
 
 
 @song_routes.route("/")
@@ -16,19 +91,3 @@ def get_all_songs():
     songs = Song.query.all()
     songs_dict = {song.id : song.to_dict() for song in songs}
     return songs_dict
-
-
-@song_routes.route("/<int:song_id>")
-@login_required
-def play_song(song_id):
-    """
-    Getting one song to play
-    """
-
-    song = Song.query.get(song_id)
-    albums = Album.query.filter(Album.id == song.album_id)
-    albums_dict = {album.id : album.to_dict() for album in albums}
-    song_dict = song.to_dict()
-    song_dict["albums"] = albums_dict
-
-    return song_dict
--- react-app/src/store/audioPlayer.js
+++ react-app/src/store/audioPlayer.js
@@ -1,9 +1,32 @@
 const PLAY_SONG = "audioPlayer/PLAY_SONG"
+const SKIP_SONG = "audioPlayer/SKIP_SONG"
+const PLAY_ALBUM = "audioPlayer/PLAY_ALBUM"
+const PLAY_ARTIST = "audioPlayer/PLAY_ARTIST"
+const PLAY_PLAYLIST = "audioPlayer/PLAY_PLAYLIST"
 
 
 const playSongAction = (song) => ({
     type: PLAY_SONG,
     song,
+})
+
+const skipSongAction = () => ({
+    type: SKIP_SONG,
+})
+
+const playAlbumAction = (songsInAlbum) => ({
+    type: PLAY_ALBUM,
+    songsInAlbum,
+})
+
+const playArtistAction = (songsByArtist) => ({
+    type: PLAY_ARTIST,
+    songsByArtist
+})
+
+const playPlaylistAction = (songsOnPlaylist) => ({
+    type: PLAY_PLAYLIST,
+    songsOnPlaylist,
 })
 
 
@@ -18,6 +41,43 @@ export const playSongThunk = (songId) => async (dispatch) => {
     }
 }
 
+export const playAlbumThunk = (albumId) => async (dispatch) => {
+    const response = await fetch(`/api/songs/album/${albumId}`)
+    const playAlbum = await response.json()
+    if (response.ok) {
+        dispatch(playAlbumAction(playAlbum))
+        return playAlbum
+    } else {
+        return playAlbum
+    }
+}
+
+export const playArtistThunk = (artistId) => async (dispatch) => {
+    const response = await fetch(`/api/songs/artist/${artistId}`)
+    const playArtist = await response.json()
+    if (response.ok) {
+        dispatch(playArtistAction(playArtist))
+        return playArtist
+    } else {
+        return playArtist
+    }
+}
+
+export const playPlaylistThunk = (playlistId) => async (dispatch) => {
+    const response = await fetch(`/api/songs/playlist/${playlistId}`)
+    const playPlaylist = await response.json()
+    if (response.ok) {
+        dispatch(playPlaylistAction(playPlaylist))
+        return playPlaylist
+    } else {
+        return playPlaylist
+    }
+}
+
+export const skipSongThunk = () => async (dispatch) => {
+    dispatch(skipSongAction())
+}
+
 
 const initialState = { currentSong : [], queue: []}
 
@@ -26,6 +86,27 @@ export default function reducer(state = initialState, action) {
     switch (action.type) {
         case PLAY_SONG:
             newState.currentSong = [action.song]
+            return newState
+        case SKIP_SONG:
+            if (newState.queue.length === 0) {
+                newState.currentSong = []
+                newState.queue = []
+            } else {
+                newState.currentSong = [state.queue[0]]
+                newState.queue = [...state.queue.slice(1)]
+                return newState
+            }
+        case PLAY_ALBUM:
+            newState.currentSong = [action.songsInAlbum[0]]
+            newState.queue = [...action.songsInAlbum.slice(1)]
+            return newState
+        case PLAY_ARTIST:
+            newState.currentSong = [action.songsByArtist[0]]
+            newState.queue = [...action.songsByArtist.slice(1)]
+            return newState
+        case PLAY_PLAYLIST:
+            newState.currentSong = [action.songsOnPlaylist[0]]
+            newState.queue = [...action.songsOnPlaylist.slice(1)]
             return newState
         default:
             return state
