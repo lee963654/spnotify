5595c5f5f8f7807d59c6af77a6bab369b60344b9
James Lee <lee963654@gmail.com>
2023-06-24 01:14:37 -0400

    need to style modals

--- react-app/src/components/PlaylistPage/index.js
+++ react-app/src/components/PlaylistPage/index.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { getAllPlaylistsThunk, getUserPlaylistsThunk, removeSongFromPlaylistThunk } from '../../store/playlists';
 import OpenModalButton from '../OpenModalButton';
 import UpdatePlaylist from '../UpdatePlaylist';
 import { getArtistsThunk } from '../../store/artists';
 import { getAlbumsThunk } from '../../store/albums';
 import { playPlaylistThunk, playSongThunk } from '../../store/audioPlayer';
+import "./PlaylistPage.css"
+import PlaylistNameModal from './PlaylistNameModal';
 
 export default function PlaylistPage() {
+    const history = useHistory()
     const dispatch = useDispatch()
     const { id } = useParams()
     const allPlaylists = useSelector(state => state?.playlists?.allPlaylists)
@@ -48,36 +51,40 @@ export default function PlaylistPage() {
 
     // On a redirect, will rerender to top of the page
     useEffect(() => {
-        window.scrollTo(0, 0);
-      }, []);
+        document.getElementById("scroll-to-top").scroll(0, 0)
+    }, []);
 
 
 
     return (
-        <div className="single-playlist-container">
+        <div className="single-playlist-page-container">
             <div className="playlist-header-container">
                 <p>Playlist</p>
-                <OpenModalButton
+                <PlaylistNameModal
                     buttonText={currentPlaylist?.name}
                     modalComponent={<UpdatePlaylist playlistName={currentPlaylist?.name} playlistId={id} playlistPrivate={playlistPrivate} />}
                 />
                 <p>{currentPlaylist?.user_playlist?.username}</p>
-                <div onClick={handleClickPlaylist}>PLAY PLAYLIST BUTTON</div>
             </div>
+            <div className="middle-play-container"><i onClick={handleClickPlaylist} class="fa-solid fa-play fa-lg"></i></div>
             <div className="playlist-song-container">
                 {songObj && songObj.length > 0 && Array.isArray(songObj) ? (songObj.map(song => (
                     <div className="playlist-song">
                         <div className="title-container">
-                        <img src={song?.album_cover} style={{width: 100}}></img>
-                        <p>{song?.name}</p>
-                        <p>{artists[song.artist_id]?.name}</p>
+                            <img src={song?.album_cover} style={{ width: 100 }}></img>
+                            <div className="playlist-song-artist">
+                                <p onClick={() => history.push(`/songs/${song.id}`)}>{song?.name}</p>
+                                <p onClick={() => history.push(`/artists/${song.artist_id}`)}>{artists[song.artist_id]?.name}</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>{albums[song.album_id]?.name}</p>
-                        </div>
-                        <div>
-                            <button onClick={(e) => {handleClickSong(e, song.id, song.album_id, song.artist_id)}}>PLAY SONG</button>
-                            <button onClick={(e) => {handleSubmit(e, song.id)}}>Remove Song</button>
+                        <div className="playlist-bar-right">
+                            <div className="playlist-album-name">
+                                <p onClick={() => history.push(`/albums/${song.album_id}`)}>{albums[song.album_id]?.name}</p>
+                            </div>
+                            <div className="playlist-controls-container">
+                                <div onClick={(e) => { handleClickSong(e, song.id, song.album_id, song.artist_id) }}><i class="fa-solid fa-play"></i></div>
+                                <div onClick={(e) => { handleSubmit(e, song.id) }}><i class="fa-regular fa-trash-can"></i></div>
+                            </div>
                         </div>
                     </div>
                 ))) : <div>No Songs</div>}
