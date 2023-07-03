d37b7fa43f2d745df497c2b640e86d7ecd8617db
James Lee <lee963654@gmail.com>
2023-07-03 19:35:13 -0400

    delete playlist modal update

--- react-app/src/components/DeletePlaylistModal/index.js
+++ react-app/src/components/DeletePlaylistModal/index.js
@@ -25,6 +25,7 @@ export default function DeletePlaylistModal({playlistId, playlistName}) {
         closeModal()
     }
 
+
     const handleCancelButton = (e) => {
         e.preventDefault()
         closeModal()
